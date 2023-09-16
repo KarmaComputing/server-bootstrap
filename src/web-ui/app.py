66c3b0eabf534a614f0217584a8ed649519a4f6f
chrisjsimpson <chris.j.simpson@live.co.uk>
2023-09-16 15:07:02 +0100

    add api_reponse

--- src/web-ui/app.py
+++ src/web-ui/app.py
@@ -3,6 +3,7 @@ import subprocess
 from flask import Flask, jsonify, render_template
 import requests
 from requests.auth import HTTPBasicAuth
+from types import SimpleNamespace
 
 app = Flask(__name__)
 
@@ -15,14 +16,31 @@ IDRAC_SCRIPTS_BASE_PATH = os.getenv(
 )
 
 
+def api_response(req):
+    error = None
+    if req.status_code not in range(200, 299):
+        error = req.text
+    return jsonify({"output": req.text, "error": error}), req.status_code
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
 def execute_redfish_command(action):
+    if action == "VerifyiDRACAccess":
+        url = f"https://{ip_address}/redfish/v1/Systems/"
+        headers = {"Content-Type": "application/json"}
+        payload = {"ResetType": "ForceRestart"}
+        auth = (username, password)
+        req = requests.get(
+            url, headers=headers, json=payload, auth=auth, verify=False
+        )  # noqa: E501
+        return api_response(req)
+
     if action == "ForceRestart":
-        url = f"https://{ip_address}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset"
+        url = f"https://{ip_address}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset"  # noqa: E501
         headers = {"Content-Type": "application/json"}
         payload = {"ResetType": "ForceRestart"}
         auth = (username, password)
@@ -30,25 +48,17 @@ def execute_redfish_command(action):
         req = requests.post(
             url, headers=headers, json=payload, auth=auth, verify=False
         )  # noqa: E501
-        return (
-            jsonify(
-                {
-                    "output": req.text,
-                    "error": req.text,
-                }
-            ),
-            req.status_code,
-        )
+        return api_response(req)
 
     if action == "MountiPXE":
         command = (
-            f'curl -v -H "Content-Type: application/json" -X POST -u {username}:{password} '
-            f"-k https://{ip_address}/redfish/v1/Managers/iDRAC.Embedded.1/VirtualMedia/CD/Actions/VirtualMedia.InsertMedia "
-            f'-d \'{{"Image": "http://138.201.59.208/sites/default/files/ipxe.iso"}}\''
+            f'curl -v -H "Content-Type: application/json" -X POST -u {username}:{password} '  # noqa: E501
+            f"-k https://{ip_address}/redfish/v1/Managers/iDRAC.Embedded.1/VirtualMedia/CD/Actions/VirtualMedia.InsertMedia "  # noqa: E501
+            f'-d \'{{"Image": "http://138.201.59.208/sites/default/files/ipxe.iso"}}\''  # noqa: E501
         )
 
         # URL
-        url = f"https://{ip_address}/redfish/v1/Managers/iDRAC.Embedded.1/VirtualMedia/CD/Actions/VirtualMedia.InsertMedia"
+        url = f"https://{ip_address}/redfish/v1/Managers/iDRAC.Embedded.1/VirtualMedia/CD/Actions/VirtualMedia.InsertMedia"  # noqa: E501
 
         # Headers
         headers = {"Content-Type": "application/json"}
@@ -64,20 +74,12 @@ def execute_redfish_command(action):
             auth=HTTPBasicAuth(username, password),
             verify=False,
         )
-        return (
-            jsonify(
-                {
-                    "output": req.text,
-                    "error": req.text,
-                }
-            ),
-            req.status_code,
-        )
+        return api_response(req)
 
     if action == "UnMountiPXE":
 
         # URL
-        url = f"https://{ip_address}/redfish/v1/Managers/iDRAC.Embedded.1/VirtualMedia/CD/Actions/VirtualMedia.EjectMedia"
+        url = f"https://{ip_address}/redfish/v1/Managers/iDRAC.Embedded.1/VirtualMedia/CD/Actions/VirtualMedia.EjectMedia"  # noqa: E501
 
         # Headers
         headers = {"Content-Type": "application/json"}
@@ -93,44 +95,7 @@ def execute_redfish_command(action):
             auth=HTTPBasicAuth(username, password),
             verify=False,
         )
-        return (
-            jsonify(
-                {
-                    "output": req.text,
-                    "error": req.text,
-                }
-            ),
-            req.status_code,
-        )
-
-    if action == "SetOnetimeBootValueVirtualCD":
-
-        # URL
-        url = f"https://{ip_address}/redfish/v1/Systems/System.Embedded.1"
-
-        # Headers
-        headers = {"Content-Type": "application/json"}
-
-        # Data
-        data = {"Boot": {"BootSourceOverrideTarget": "None"}}
-
-        # Making the request
-        req = requests.patch(
-            url,
-            headers=headers,
-            json=data,
-            auth=HTTPBasicAuth(username, password),
-            verify=False,
-        )
-        return (
-            jsonify(
-                {
-                    "output": req.text,
-                    "error": req.text,
-                }
-            ),
-            req.status_code,
-        )
+        return api_response(req)
 
     if action == "GetOnetimeBootValue":
 
@@ -143,26 +108,34 @@ def execute_redfish_command(action):
             auth=HTTPBasicAuth(username, password),
             verify=False,
         )
-        return (
-            jsonify(
-                {
-                    "output": req.text,
-                    "error": req.text,
-                }
-            ),
-            req.status_code,
-        )
+        return api_response(req)
 
     if action == "GetPowerState":
-        command = f"python {IDRAC_SCRIPTS_BASE_PATH}GetSetPowerStateREDFISH.py -ip {ip_address} -u {username} -p {password} --get"  # noqa: E501
-        result = subprocess.run(command, capture_output=True, shell=True)
+        # URL
+        url = f"https://{ip_address}/redfish/v1/Systems/System.Embedded.1"
+
+        # Making the request
+        req = requests.get(
+            url,
+            auth=HTTPBasicAuth(username, password),
+            verify=False,
+        )
+        if req.status_code == 200:
+            response_text = (
+                f"Current server power state: {req.json()['PowerState']}"  # noqa: E501
+            )
+            req = SimpleNamespace()
+            req.status_code = 200
+            req.text = response_text
+        return api_response(req)
+
     if action == "ChangeBiosBootOrderREDFISH":
         command = f"python {IDRAC_SCRIPTS_BASE_PATH}ChangeBiosBootOrderREDFISH.py -ip {ip_address} -u {username} -p {password} --get"  # noqa: E501
         result = subprocess.run(command, capture_output=True, shell=True)
 
     if action == "On":
         # URL
-        url = f"https://{ip_address}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset"
+        url = f"https://{ip_address}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset"  # noqa: E501
 
         # Headers
         headers = {"Content-Type": "application/json"}
@@ -178,19 +151,11 @@ def execute_redfish_command(action):
             auth=HTTPBasicAuth(username, password),
             verify=False,
         )
-        return (
-            jsonify(
-                {
-                    "output": req.text,
-                    "error": req.text,
-                }
-            ),
-            req.status_code,
-        )
+        return api_response(req)
 
     if action == "ForceOff":
         # URL
-        url = f"https://{ip_address}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset"
+        url = f"https://{ip_address}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset"  # noqa: E501
 
         # Headers
         headers = {"Content-Type": "application/json"}
@@ -206,15 +171,7 @@ def execute_redfish_command(action):
             auth=HTTPBasicAuth(username, password),
             verify=False,
         )
-        return (
-            jsonify(
-                {
-                    "output": req.text,
-                    "error": req.text,
-                }
-            ),
-            req.status_code,
-        )
+        return api_response(req)
 
     else:
         command = f"python {IDRAC_SCRIPTS_BASE_PATH}GetSetPowerStateREDFISH.py -ip {ip_address} -u {username} -p {password} --set {action}"  # noqa: E501
@@ -228,6 +185,11 @@ def execute_redfish_command(action):
         ),
         result.returncode,
     )
+
+
+@app.route("/api/v1/VerifyiDRACAccess", methods=["POST"])
+def verify_idrac_access():
+    return execute_redfish_command("VerifyiDRACAccess")
 
 
 @app.route("/api/v1/On", methods=["POST"])
@@ -273,11 +235,6 @@ def unmount_iPXE_iso():
 @app.route("/api/v1/GetOnetimeBootValue", methods=["POST"])
 def get_current_onetime_boot_order():
     return execute_redfish_command("GetOnetimeBootValue")
-
-
-@app.route("/api/v1/SetOnetimeBootValueVirtualCD", methods=["POST"])
-def get_boot_from_virtual_cd():
-    return execute_redfish_command("SetOnetimeBootValueVirtualCD")
 
 
 if __name__ == "__main__":
