21c87cae525cb6d11123f8e36335d152686756f4
dysen <denisdysen@proton.me>
2025-05-16 19:29:45 +0100

    Added environment variable handling and replaced the values used for NewServer and the wipe loop as appropriate

--- internal/controller/main.go
+++ internal/controller/main.go
@@ -1,17 +1,54 @@
-// SPDX-License-Identifier: BSD-3-Clause
 package main
 
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
-	server, err := NewServer("https://192.168.0.230", "Administrator", "A0F7HKUU", false)
+	urlAddress := os.Getenv("URL")
+	if urlAddress == "" {
+		log.Fatal("URL is not set, expects format e.g. 'https://192.168.0.230'")
+	}
+	username := os.Getenv("USERNAME")
+	if username == "" {
+		log.Fatal("USERNAME is not set, expects string")
+	}
+	password := os.Getenv("PASSWORD")
+	if password == "" {
+		log.Fatal("PASSWORD is not set, expects string")
+	}
+	validCert := os.Getenv("VALIDCERT")
+	if validCert == "" {
+		log.Fatal("VALIDCERT is not set, expects boolean")
+	}
+	var isValidCert bool
+	switch strings.ToLower(validCert) {
+	case "true":
+		isValidCert = true
+	case "false":
+		isValidCert = false
+	default:
+		log.Fatal("validCert is not <true|false>")
+	}
+	wipeInterval := os.Getenv("WIPEINTERVAL")
+	if wipeInterval == "" {
+		log.Fatal("WIPEINTERVAL is not set, expects seconds in an integer")
+	}
+	wipeIntervalInt, err := strconv.Atoi(wipeInterval)
+	if err != nil {
+		log.Fatal("WIPEINTERVAL is not an integer")
+	}
+
+	server, err := NewServer(urlAddress, username, password, isValidCert)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer server.Destroy()
 
 	for {
@@ -23,8 +60,7 @@ func main() {
 			time.Sleep(1 * time.Second)
 			fmt.Println("Waiting for power state to change...")
 		}
-
-		fmt.Println("Waiting for 10 minutes...")
-		time.Sleep(10 * time.Minute)
+		fmt.Printf("Waiting for %d seconds\n", wipeIntervalInt)
+		time.Sleep(time.Duration(wipeIntervalInt))
 	}
 }
