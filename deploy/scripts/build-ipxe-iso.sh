3d0c6e3f152bc4fc4f45816d398fb3d1e9709ccc
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2025-05-22 23:46:11 +0100

    Fix SSH key generation

--- deploy/scripts/build-ipxe-iso.sh
+++ deploy/scripts/build-ipxe-iso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-FILE="vm-test.ipxe"
+FILE="alpinebooter.ipxe"
 
 WWW_DIR="../serve/www"
 BUILD_DIR="../build/ipxe"
@@ -11,6 +11,9 @@ ISO_MAKE_THREADS=16
 
 echo "--- Creating serve directory at ${WWW_DIR} ---"
 mkdir -p ${WWW_DIR}
+
+echo "--- Creating ssh key directory at ${SSH_KEY_DIR} ---"
+mkdir -p ${SSH_KEY_DIR}
 
 echo "--- Generating new SSH key pair in ${SSH_KEY_DIR} ---"
 ssh-keygen -t rsa -f ${SSH_KEY_DIR}/key -N ""
@@ -22,11 +25,15 @@ mv -f ${SSH_KEY_DIR}/key.pub ${WWW_DIR}/ssh/key.pub
 echo "--- Setting correct permissions for private key ---"
 chmod 600 ${SSH_KEY_DIR}/key
 
-echo "--- Building ${PODMAN_IMAGE_NAME} ---"
-podman build \
-    --tag ${PODMAN_IMAGE_NAME} \
-    --no-cache \
-    ${BUILD_DIR}
+# echo "--- Building ${PODMAN_IMAGE_NAME} ---"
+# podman build \
+#     --tag ${PODMAN_IMAGE_NAME} \
+#     ${BUILD_DIR}
+
+if [ $? -neq 0 ]; then
+  echo "!!! BUILD FAILED, EXITING !!!"
+  exit
+fi
 
 echo "--- Building ipxe.iso with ${FILE} embedded, writing to ${WWW_DIR} (using ${ISO_MAKE_THREADS} threads) ---"
 podman run \
