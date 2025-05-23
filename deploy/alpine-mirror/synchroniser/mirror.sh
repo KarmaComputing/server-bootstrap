1aa6379bce8d3bac175924c40c3f8e2bbd5b1a69
dysen <denisdysen@proton.me>
2025-05-23 17:30:25 +0100

    Added helpful echos and removed unnecessary comment

--- deploy/alpine-mirror/synchroniser/mirror.sh
+++ deploy/alpine-mirror/synchroniser/mirror.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-    
+
 # make sure we never run 2 rsync at the same time
 lockfile="/tmp/alpine-mirror.lock"
 if [ -z "$flock" ] ; then
@@ -9,8 +9,9 @@ fi
 src=rsync://rsync.alpinelinux.org/alpine/ 
 dest=/site
 
-# uncomment this to exclude old v2.x branches
 exclude="--exclude v2.* --exclude v3.0 --exclude v3.1 --exclude v3.2 --exclude v3.3 --exclude v3.4 --exclude v3.5 --exclude v3.6 --exclude v3.7 --exclude v3.8 --exclude v3.9 --exclude v3.10 --exclude v3.11 --exclude v3.12 --exclude v3.13 --exclude v3.14 --exclude v3.15 --exclude v3.16 --exclude v3.17"
+
+echo "--- Starting Sync ---"
 
 mkdir -p "$dest"
 /usr/bin/rsync \
@@ -23,3 +24,5 @@ mkdir -p "$dest"
         --timeout=600 \
         $exclude \
         "$src" "$dest"
+        
+echo "--- Finished Sync ---"
